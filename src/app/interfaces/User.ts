8bf12da862b0a00da0f9cafec036a515d4f44029
PawanBirYadav <pawanbiryadav@gmail.com>
2021-08-19 16:17:31 +0530

    Initial commit

---
+++ src/app/interfaces/User.ts
@@ -0,0 +1,14 @@
+
+export interface IUser {
+
+  firstName: string ;
+  lastName: string ;
+  emailId: string;
+  roleId: number;
+  userPassword: string ;
+  gender: string ;
+  contactNumber: number ;
+  dateOfBirth: Date;
+  address: string ;
+
+}
